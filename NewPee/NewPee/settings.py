1983de76cf3b41d32d72284efa2aedfe63e9b395
Fangting Chen <fc1@ualberta.ca>
2019-02-13 17:11:13 -0700

    done

--- NewPee/NewPee/settings.py
+++ NewPee/NewPee/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
--- NewPee/posts/models.py
+++ NewPee/posts/models.py
@@ -1,38 +1,103 @@
 from django.db import models
-#from users.models import Users 
+from django.contrib.postgres.fields import JSONField
+import json
+import datetime
 
 # Create your models here.
 class Posts(models.Model):
     #have to change to a user model
-    author = models.CharField(max_length=250)
+    author = JSONField()
     #author = models.ForeignKey(User)
-    title = models.CharField(max_length=30)
-    #have to figure out how to render markdown 
-    body = models.TextField()
-    images = ArrayField(models.URLField())
-    # comments = ArrayField(models.CharField(max_length=140))
 
+    title = JSONField()
+    #have to figure out how to render markdown
+    body = JSONField()
 
-    def write_title(self,title):
-        self.title = title 
-        return 
+    #will be stored as '{"images":[url1, url2,...]}'
+    images = JSONField()
 
-    def get_title(self):
-        return self.title 
+    #"{comments:[user1:comment1, user2:comment2,...]}""
+    comments = JSONField()
 
-    def write_body(self,body):
-        self.body = body
-        return
-
-    def get_body(self):
-        return self.body
+    #'{"like":0, "dislike":0}'
+    votes = JSONField()
+    timestamp = json.dumps(str(datetime.datetime.now()))
     
-    def add_image(self,image_url):
-        self.images.append(image_url)
-        return
+    def __init__(self):
+        self.images = json.dumps({"images":[]})
+        self.comments = json.dumps({"comments":[]})
+        self.votes = json.dumps({"like": 0, "dislike": 0})
 
-    def get_images(self):
-        return self.images
 
-    # def add_comment(self,comment):
-    #     self.comments.append(comment)
+    #pass in author object in json 
+    def setAuthor(self,author):
+        self.author = author
+
+    def getAuthor(self):
+        return self.author
+
+    def writeTitle(self,title):
+        self.title = json.dumps(title)
+
+    #return string 
+    def getTitle(self):
+        title = json.loads(self.title)
+        return title 
+
+    def writeBody(self,body):
+        self.body = json.dumps(body)
+
+    def getBody(self):
+        body = json.loads(self.body)
+        return body
+    
+    def addImage(self,image_url):
+        stringImages = json.loads(self.images)
+        stringImages['images'].append(image_url)
+        self.images = json.dumps(stringImages)
+
+    #returns a list of images 
+    def getAllImages(self):
+        stringImages = json.loads(self.images)
+        images = stringImages['images']
+        return images
+
+    #return a single url for the image 
+    def getImage(self,index):
+        stringImages = json.loads(self.images)
+        image = stringImages['images'][index]
+        return image
+        
+
+    def addComment(self,username, comment):
+        stringComments = json.loads(self.comments)
+        commentInfo = username + ":" + comment
+        stringComments['comments'].append(commentInfo)
+        self.comments = json.dumps(stringComments)
+
+    def getAllComments(self):
+        stringComments = json.loads(self.comments)
+        comments = stringComments['comments']
+        return comments
+        
+    #return username and comment 
+    def getComment(self,index):
+        stringComments = json.loads(self.comments)
+        comment = stringComments['comments'][index]
+        username = comment.split(":")[0]
+        commentText = comment.split(":")[1]
+        return username, commentText
+
+    def like(self):
+        stringVotes = json.loads(self.votes)
+        likes = stringVotes['like']
+        likes += 1 
+        stringVotes['like'] = likes
+        self.votes = json.dumps(stringVotes)
+
+    def dislike(self):
+        stringVotes = json.loads(self.votes)
+        dislikes = stringVotes['dislike']
+        dislikes += 1 
+        stringVotes['dislike'] = dislikes
+        self.votes = json.dumps(stringVotes)
