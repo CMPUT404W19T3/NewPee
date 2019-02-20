fd9b951359d46161829d01fb08a6aa46a559bb0b
Student <jblazusi@ualberta.ca>
2019-02-20 02:20:48 -0500

    Adjusted installed apps

--- NewPee/NewPee/settings.py
+++ NewPee/NewPee/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+
+    # My App
+    'author',
+    'posts',
 ]
 
 MIDDLEWARE = [
