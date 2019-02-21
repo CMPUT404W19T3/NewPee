e672444a681e770e7fab6f1d80cfd3d78e997aa7
Matthew Kluk <mkluk@ualberta.ca>
2019-02-20 21:02:25 -0500

    Sorry about the spam, I just wanted to ensure my work was saved

--- NewPee/testAuthor.py
+++ NewPee/testAuthor.py
@@ -1,2 +1,6 @@
 from django.test import TestCase
 from models import author
+
+class AuthorModelTests(TestCase):
+    def setUp(self):
+        pass
--- NewPee/testPosts.py
+++ NewPee/testPosts.py
@@ -1,2 +1,10 @@
 from django.test import TestCase
 from models import posts
+
+class PostModelTests(TestCase):
+    def setUp(self):
+        pass
+
+class CommentModelTests(TestCase):
+    def setUp(self):
+        pass
