1852c0175c952b3954dce077e24aa43e33fe24c5
Fangting Chen <fc1@ualberta.ca>
2019-03-05 20:13:38 -0700

    made header template

--- NewPee/NewPee/urls.py
+++ NewPee/NewPee/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
 from views.author_views import log_in, sign_up
+from views.general_views import header
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', log_in),
     path('signup/', sign_up),
+    path('header/', header)
 ]
