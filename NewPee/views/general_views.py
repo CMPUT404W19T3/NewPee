001891e1ab91d0610e68760a06a75a3e6117cbcb
mjenius <jamaledd@ualberta.ca>
2019-03-12 22:11:02 -0600

    Added Login & Signup Views

--- NewPee/views/general_views.py
+++ NewPee/views/general_views.py
@@ -8,3 +8,9 @@ def header(request, format=None):
   
 def home(request, format=None):
     return render(request, 'homepage.html', {})
+
+def log_in(request, format=None):
+    return render(request, 'login.html', {})
+
+def sign_up(request, format=None):
+    return render(request, 'signup.html', {})
