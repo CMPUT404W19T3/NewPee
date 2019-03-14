b1cfc68f41e0f932a63fb7f2568ef15f829ce66d
Matthew Kluk <spongebob1894@hotmail.com>
2019-03-14 16:09:59 -0600

    still trying to write a search function

--- NewPee/static/scripts/search.js
+++ NewPee/static/scripts/search.js
@@ -1,5 +1,8 @@
 function getPosts(searchTerm) {
     var promise = fetch('../api/posts');
+    promise.then((getJSON) => {
+        return getJSON.json();
+    })
 }
 
 function getAuthors(searchTerm) {
