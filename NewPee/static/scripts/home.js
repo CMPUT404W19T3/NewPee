8ea26907a3b7a61c5cb67d1239c323f113e2b556
Fangting Chen <fc1@ualberta.ca>
2019-03-25 12:38:27 -0600

    factored out getCookie fucntion

--- NewPee/static/scripts/home.js
+++ NewPee/static/scripts/home.js
@@ -1,4 +1,4 @@
-
+import { getCookie } from 'cookie'; 
 
 
 $(document).ready(function(){
@@ -12,24 +12,6 @@ const follow_submit_button = document.querySelector("#follow_user_submit_button"
 var page_author;
 var user_author;
 
-
-
-//https:docs.djangoproject.com/en/dev/ref/csrf/#ajax
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
 
 function getFileData(myFile){
