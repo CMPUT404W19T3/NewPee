39d6155e23fb7459441033bc7f3c81f3c9b813b6
Student <jblazusi@ualberta.ca>
2019-03-24 22:52:37 -0400

    migrations

--- NewPee/Posts/models.py
+++ NewPee/Posts/models.py
@@ -14,7 +14,8 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     # have to change to a user model
-    author = models.CharField(max_length=140, null=False,blank=False)
+    author = models.ForeignKey('Post', on_delete=models.CASCADE , null=False, blank=False)
+    #author = models.CharField(max_length=140, null=False,blank=False)
     # author = models.ForeignKey(User)
 
     title = models.CharField(max_length=30, null=False, blank=False)
