84c092524479a409fbf58d3c799b80b394df54c2
Student <jblazusi@ualberta.ca>
2019-03-28 00:31:35 -0400

    fix travis errors?

--- NewPee/NewPee/settings.py
+++ NewPee/NewPee/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,4 +135,6 @@ STATICFILES_DIRS = [
 
 LOGOUT_REDIRECT_URL = 'login'
 
-django_heroku.settings(locals())
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
