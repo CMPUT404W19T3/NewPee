df4c7c0335211fbbf49ac6ac4b116ed6944ee483
Justin Widney <jwidney@cs.ualberta.ca>
2019-04-04 12:42:36 -0600

    authors page broken.

--- NewPee/Authors/views.py
+++ NewPee/Authors/views.py
@@ -116,6 +116,9 @@ class AuthorDetail(APIView):
 
                 print(filtered)
 
+                #filtered_serializer = PostSerializer(filtered, many=True,context={'request': request})
+
+
                 allPosts = list(chain(filtered, foreignposts_serializer.data))
                 allPosts.sort(key=lambda x: x['post_date'], reverse=True)
                 
