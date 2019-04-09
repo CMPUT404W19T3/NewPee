31a1b30b2dcdbf3534f136a37d421b85f37a0b77
Fangting Chen <fc1@ualberta.ca>
2019-04-08 18:37:56 -0600

    testsss

--- NewPee/Authors/models.py
+++ NewPee/Authors/models.py
@@ -28,7 +28,6 @@ class Author(models.Model):
     following = models.ManyToManyField("self", related_name="_following", symmetrical=False, blank=True)
     followers = models.ManyToManyField("self", related_name="_followers", symmetrical=False, blank=True)
     friend_requests = models.ManyToManyField("self", related_name="_friend_requests", symmetrical=False, blank=True)
-
     Admin = models.BooleanField(default=False)
 
     # Only Admin can Change.
@@ -38,11 +37,8 @@ class Author(models.Model):
     def __str__(self):
 
         try:
-
             return self.user.get_username()
-
         except:
-
             return self.displayName
 
     # All "get" functions for username, password, email, etc... are inherited from Django User
@@ -146,12 +142,6 @@ class Author(models.Model):
         if(author not in self.followers.all()): # make sure we are in reciever followers.
             self.followers.add(author)
 
-
-
-        print(self, "author with host")
-        print(author, "the author being sent")
-
-
         if(author.host != HOSTNAME):
             try:
                 self.send_foreign_request(author) # send a friend request to another serve
@@ -174,6 +164,7 @@ class Author(models.Model):
     def add_friend_request(self, author):
 
         self.friend_requests.add(author)
+        self.save()
 
     def get_declined_requests(self):
 
@@ -214,13 +205,7 @@ class Author(models.Model):
         #request = session.post(url = "http://127.0.0.1:8000/api/friendrequest", data=  PARAMS )
 
 
-        
-
-
-
-
-
-
+    
     # we have recieved a friend request from the author
     def send_friend_request(self, author):
 
--- NewPee/Tests/test_post.py
+++ NewPee/Tests/test_post.py
@@ -26,14 +26,6 @@ class PostModelTests(TestCase):
 
         self.assertEqual(test_author, test_fetch_author)
 
-    def test_set_image(self):
-
-        test_post = Post()
-        image_url = "https://i.ytimg.com/vi/rNxih0ikkDo/maxresdefault.jpg"
-        test_post.set_image(image_url)
-        # self.assertIsNotNone(test_post.get_image())
-        self.assertEqual(test_post.get_image(), image_url)
-
     def test_post_date(self):
 
         test_post = Post(post_date=datetime.datetime.now())
@@ -49,27 +41,157 @@ class PostModelTests(TestCase):
 
     def test_make_private_to_me(self):
 
-        pass
-
-    def test_make_private_to_author(self):
-
         test_author = Author()
         test_post = Post(author=test_author, visibility="PRIVATE")
 
         self.assertEqual(test_post.visibility, "PRIVATE")
         
+    def test_set_visible_to(self):
+        author1 = Author()
+        author2 = Author()
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        #setting testpost to be visible to author2 
+        test_post.set_visible_to(author2)
+        test_post.save()
+        self.assertEqual(test_post.visible_to.all()[0], author2)
 
-    def test_make_private_to_friends(self):
 
-        pass
+    def test_privateViewAccess_true(self):
+        author1 = Author()
+        author2 = Author()
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        test_post.set_visible_to(author2)
+        test_post.save()
+        self.assertTrue(test_post.privateViewAccess(author2))
 
-    def test_make_private_to_friends_of_friends(self):
+    def test_privateViewAccess_false(self):
+        author1 = Author()
+        author2 = Author()
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.privateViewAccess(author2))
 
-        pass
+    def test_friendViewAccess_true(self):
+        author1 = Author()
+        author2 = Author()
+        author1.save()
+        author2.save()
+        author1.friends.add(author2)
+        author2.friends.add(author1)
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertTrue(test_post.friendViewAccess(author2))
 
-    def test_make_private_to_host_friends(self):
+    def test_friendViewAccess_false(self):
+        author1 = Author()
+        author2 = Author()
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.friendViewAccess(author2))
 
-        pass
+
+    def test_FriendServerViewAcess_true(self):
+        author1 = Author(host="http://newpee.herokuapp.com/")
+        author2 = Author(host="http://newpee.herokuapp.com/")
+        author1.save()
+        author2.save()
+        author1.friends.add(author2)
+        author2.friends.add(author1)
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertTrue(test_post.FriendServerViewAcess(author2))
+
+    def test_FriendServerViewAcess_false_not_friend_same_server(self):
+        author1 = Author(host="host1")
+        author2 = Author(host="host1")
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.FriendServerViewAcess(author2))
+    
+    def test_FriendServerViewAcess_false_friend_not_same_server(self):
+        author1 = Author(host="host1")
+        author2 = Author(host="host2")
+        author1.save()
+        author2.save()
+        author1.friends.add(author2)
+        author2.friends.add(author1)
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.FriendServerViewAcess(author2))
+
+    def test_FOAFViewAccess_true(self):
+        author1 = Author()
+        author2 = Author()
+        author3 = Author()
+        author1.save()
+        author2.save()
+        author3.save()
+        #author 1 is frineds with 2 
+        author1.friends.add(author2)
+        author2.friends.add(author1)
+        #author 2 is frineds with 3
+        author2.friends.add(author3)
+        author3.friends.add(author2)
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertTrue(test_post.FOAFViewAccess(author3))
+
+    def test_FOAFViewAccess_false(self):
+        author1 = Author()
+        author2 = Author()
+        author3 = Author()
+        author1.save()
+        author2.save()
+        author3.save()
+        #author 1 is frineds with 2 
+        author1.friends.add(author2)
+        author2.friends.add(author1)
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.FOAFViewAccess(author3))
+
+    def test_ServerViewAcces_true(self):
+        author1 = Author(host="http://newpee.herokuapp.com/")
+        author2 = Author(host="http://newpee.herokuapp.com/")
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertTrue(test_post.ServerViewAcces(author2))
+
+    def test_ServerViewAcces_false(self):
+        author1 = Author(host="http://newpee.herokuapp.com/")
+        author2 = Author(host="testhost")
+        author1.save()
+        author2.save()
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.ServerViewAcces(author2))
+
+    def test_unlisted_false(self):
+        author1 = Author(host="http://newpee.herokuapp.com/")
+        author1.save()
+        test_post = Post(author=author1)
+        test_post.save()
+        self.assertFalse(test_post.getUnlisted())
+
+    def test_unlisted_true(self):
+        author1 = Author(host="http://newpee.herokuapp.com/")
+        author1.save()
+        test_post = Post(author=author1, unlisted=True)
+        test_post.save()
+        self.assertTrue(test_post.getUnlisted())
+
 
     def test_make_public(self):
 
@@ -78,13 +200,6 @@ class PostModelTests(TestCase):
 
         self.assertEqual(test_post.visibility, "PUBLIC")
 
-    def test_body_plaintext(self):
-
-        pass
-
-    def test_body_markdown(self):
-
-        pass
 
 class CommentModelTests(TestCase):
 
